8ccbe26a5ebc7b1cf43115c4537398bde4a8279d
Vlad Rimsha <vlad@jelly.ninja>
2019-06-04 21:26:49 +0700

    Add Python 3

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ def main():
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Internet :: Name Service (DNS)',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: Utilities',
